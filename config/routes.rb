be62b76a21306c1855f891540b7e4ff88e67221d
Daan <d.smit@uva.nl>
2013-09-01 12:28:44 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Attendance::Application.routes.draw do
-  get "attendance/index"
-  get "attendance/update"
+  root 'attendance#index'
+
+  get 'attendance' => 'attendance#index'
+  post 'attendance' => 'attendance#update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
