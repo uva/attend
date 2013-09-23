1d817decc755522344883edc69c4c3b46f779204
Daan Smit <d.smit@uva.nl>
2013-09-23 15:10:05 +0200

    fixed charts, included current d3 version in assets

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -6,94 +6,106 @@ function draw_bar_chart_on(element_id, attendance_data)
 		return timeslots.map(function(slot) { return day + " " + slot; });
 	});
 
-	var max_students = 0;
-	var chart_data = timeslots_during_week.map(function(timeslots) {
-		return timeslots.map(function(timeslot) {
-			var students = 0;
-			if (attendance_data[timeslot] != undefined) {
-				students = attendance_data[timeslot];
+	var chart_data = weekdays.map(function(day) {
+		return timeslots.map(function(slot) {
+			var nr_students = 0;
+			var key = day + " " + slot;
+			if (key in attendance_data) {
+				nr_students = attendance_data[key];
 			}
-			if (students > max_students) {
-				max_students = students;
-			}
-			return { timeslot: timeslot, students: students };
+			return { day: day, timeslot: slot, students: nr_students }; 
 		});
 	});
 
+	var student_counts = [].concat.apply([], chart_data).map(function(datum) {
+		return datum.students;
+	});
+	var max_students = Math.max.apply([], student_counts);
+
+	if (max_students == 0) {
+		$(element_id).append('<p>No attendance data.</p>');
+		return;
+	}
+
 	var margin = {top: 20, right: 20, bottom: 30, left: 60},
-   width = 1600 - margin.left - margin.right,
-   height = 500 - margin.top - margin.bottom;
+	    width = 1600 - margin.left - margin.right,
+	    height = 500 - margin.top - margin.bottom;
 
-	var xDays = d3.scale.ordinal()
-		.domain(weekdays)
-		.rangeRoundBands([0, width]);
+	var scaleDays = d3.scale.ordinal()
+		    .domain(weekdays)
+		    .rangeRoundBands([0, width]);
 
-	var xTimeslots = d3.scale.ordinal()
-		.domain(d3.range(timeslots.length))
-		.rangeRoundBands([0, xDays.rangeBand()]);
+	var scaleTimeslots = d3.scale.ordinal()
+		    .domain(timeslots)
+		    .rangeRoundBands([0, scaleDays.rangeBand()]);
 
 	var y = d3.scale.linear()
-	   .domain([0, max_students])
-		.range([height, 0]);
+		    .domain([0, max_students])
+		    .range([height, 0]);
 
 	var xAxis = d3.svg.axis()
-		.scale(xTimeslots)
-		.orient("bottom");
+		    .scale(scaleTimeslots)
+		    .orient("bottom");
 
 	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left");
+		    .scale(y)
+		    .orient("left");
 
 	var chart = d3.select(element_id)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-		.append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		    .append("svg")
+		    .attr("width", width + margin.left + margin.right)
+		    .attr("height", height + margin.top + margin.bottom)
+		    .append("g")
+		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	var days = chart.selectAll("g")
-      .data(chart_data)
-		.enter().append("g")
-      .attr("class", "days")
-      .attr("transform", function(d, i) { return "translate(" + xDays(i) + ")"; });
+		    .data(chart_data)
+		    .enter().append("g")
+		    .attr("class", "days")
+		    .attr("transform", function(d) { return "translate(" + scaleDays(d[0].day) + ")"; });
 
 	chart.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
+		.attr("class", "y axis")
+		.call(yAxis)
 		.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "-3.71em")
-      .style("text-anchor", "end")
-      .text("Students");
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", "-3.71em")
+		.style("text-anchor", "end")
+		.text("Students");
 
 	chart.append("g")
-      .attr("class", "grid")
-      .call(yAxis
-            .tickSize(-width, 0, 0)
-            .tickFormat(""));
+		.attr("class", "grid")
+		.call(yAxis
+		      .tickSize(-width, 0, 0)
+		      .tickFormat(""));
 
 	var slots = days.selectAll("rect")
-      .data(function(d) { return d; })
-		.enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return xTimeslots(d.timeslot); })
-      .attr("y", function(d) { return y(d.students); })
-      .attr("width", xTimeslots.rangeBand() - 2)
-      .attr("height", function(d) { return height - y(d.students); });
+		    .data(function(d) { return d; })
+		    .enter().append("rect")
+		    .attr("class", "bar")
+		    .attr("x", function(d) { return scaleTimeslots(d.timeslot); })
+		    .attr("y", function(d) { return y(d.students); })
+		    .attr("width", scaleTimeslots.rangeBand() - 2)
+		    .attr("height", function(d) {
+			    // if (d.students > 0) {
+				 //    console.log(d.students);
+			    // }
+			    return height - y(d.students);
+		    });
 
 	days.append("g")
-      .attr("class", "x axis")
-      .attr("transform", function(d, i) { return "translate(0," + height + ")"; })
-      .each(function(d, i) {
+		.attr("class", "x axis")
+		.attr("transform", function(d, i) { return "translate(0," + height + ")"; })
+		.each(function(d, i) {
 			var scale = d3.scale.ordinal()
-				.domain(d.map(function(slot) { return slot.timeslot; }))
-				.rangeRoundBands([0, xDays.rangeBand()]);
+				    .domain(d.map(function(slot) { return slot.timeslot; }))
+				    .rangeRoundBands([0, scaleDays.rangeBand()]);
 
 			var xAxis = d3.svg.axis()
-				.scale(scale)
-				.orient("bottom");
+				    .scale(scale)
+				    .orient("bottom");
 			
 			d3.select(this).call(xAxis);
-      });
+		});
 }
